083e08fd4e4adb4e96d4ef4b774830e6eef3fcd1
pbzweihander <sd852456@naver.com>
2017-01-25 13:25:55 +0900

    대충 이렇게 하면 완성같은데 쓸 데도 없고.. 의미업다

--- crawler.py
+++ crawler.py
@@ -3,12 +3,14 @@ from bs4 import BeautifulSoup
 import random
 from pathlib import Path
 import json
+import time
 
 random.seed()
 
-prevurl = "http://namu.wiki/random"
+next = "http://namu.wiki/random"
 
 data = {}
+links = []
 
 path = Path('./data.json')
 if path.is_file():
@@ -16,9 +18,20 @@ if path.is_file():
         data = json.load(f)
 
 while True:
-    r = requests.get(prevurl)
+    r = requests.get(next)
     soup = BeautifulSoup(r.text, 'html.parser')
-    print([link.get('href') for link in soup.find_all('a')
-        if link.get('href') and '/w/' in link.get('href') and '.jpg' not in link.get('href')])
-    break
+    title = soup.title.text.replace(' - 나무위키', '')
+    if '파일:' in title:
+        next = "http://namu.wiki" + random.choice(links)
+        continue
+    links = [link.get('href') for link in soup.find_all('a')
+        if link.get('href') and '/w/' in link.get('href') and '.jpg' not in link.get('href')]
+    if title in data:
+        data[title] = data[title] + 1
+    else:
+        data[title] = 1
+    with path.open('w') as f:
+        json.dump(data, f)
+    next = "http://namu.wiki" + random.choice(links)
+    print(title)
 
